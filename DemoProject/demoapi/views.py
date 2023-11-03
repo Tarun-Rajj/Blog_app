0d8d75aafa563e065ac529ee65bdd2ddeb4c893d
Tarun-Rajj <tarunkumar86437@gmail.com>
2023-11-03 15:56:58 +0530

    My first api and it's url is 127.0.0.1:8000/demoapi/first/

--- DemoProject/demoapi/views.py
+++ DemoProject/demoapi/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(http_method_names=['GET'])
+def view_first_api(request):
+    resp=Response(data="This is my FirstAPI")
+    return resp
 
 # Create your views here.
